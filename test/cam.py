a7332b19fbd405373692889fcc6c5a927c4ec716
Marcin Przewięźlikowski <m.przewie@gmail.com>
2017-11-16 16:21:24 +0100

    MNIST PoC [1]

--- test/cam.py
+++ test/cam.py
@@ -1,4 +1,14 @@
 from pygame import camera, image
+import pygame
+import numpy as np
+
+
+def grayscale(img):
+    arr = pygame.surfarray.array3d(img)
+    #luminosity filter
+    avgs = [[(r*0.298 + g*0.587 + b*0.114) for (r,g,b) in col] for col in arr]
+    arr = np.array([[[avg,avg,avg] for avg in col] for col in avgs])
+    return pygame.surfarray.make_surface(arr)
 
 camera.init()
 cam = None
@@ -8,5 +18,18 @@ while cam is None:
 
 cam.start()
 img = cam.get_image()
+
+resized = pygame.transform.scale(img, (28,28))
 image.save(img, "photo.bmp")
+
+image.save(resized, "resized.bmp")
+
+gr = grayscale(resized)
+
+
+image.save(gr, "greyscale.bmp")
+
+print(np.array([[gr.get_at((i, j))[0] for j in range(28)] for i in range(28)]))
+
+
 camera.quit()
